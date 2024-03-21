9621b4de69e6f11440ca171f093332227eadf97b
Gurusidda Hanamannavr <143526836+dreamboyguru@users.noreply.github.com>
2024-03-21 11:39:38 +0530

    Add files via upload

---
+++ src/components/Certificates.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function Certificates() {
+  return (
+    <section id='certificates'>
+      <div className='h-auto'>
+        <div className='px-5 md:px-10 lg:px-20'>
+            <div>
+                <h1 className='text-3xl font-bold bg-white my-10 '>CERTIFICATES</h1>
+            </div>
+            <div className='flex flex-wrap max-md:flex-wrap justify-between pb-5'>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+                <div className='w-[15%] mb-5 h-40 m-1 rounded-md bg-blue-100 shadow-md'></div>
+            </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Certificates
