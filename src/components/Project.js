39a4de63fbbad2f1e63ed6f99bff95e8aba0cb7e
Gurusidda Hanamannavr <143526836+dreamboyguru@users.noreply.github.com>
2024-05-12 20:06:56 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,6 +8,7 @@ function Project() {
   const [expanded2, setExpanded2] = useState(false);
   const [expanded3, setExpanded3] = useState(false);
   const [expanded4, setExpanded4] = useState(false);
+  const [expanded5, setExpanded5] = useState(false);
 
   const handleButtonClick = (num) => {
     if(num === 1) {
