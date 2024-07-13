139473be5f9b59b8b7b6dc4a37aeeccb402c5abf
dreamboyguru <guruhanamannavar870@gmail.com>
2024-07-13 23:01:09 +0530

    add LSp project headline

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,6 +12,7 @@ function Project() {
   const [expanded3, setExpanded3] = useState(false);
   const [expanded4, setExpanded4] = useState(false);
   const [expanded5, setExpanded5] = useState(false);
+  const [expanded6, setExpanded6] = useState(false);
 
   const handleButtonClick = (num) => {
     if(num === 1) {
@@ -24,6 +25,8 @@ function Project() {
       setExpanded4(!expanded4)
     }else if( num === 5 ) {
       setExpanded5(!expanded5)
+    }else if( num === 6 ) {
+      setExpanded6(!expanded6)
     }
   };
   return (
@@ -423,6 +426,106 @@ function Project() {
                   <button type='submit' onClick={()=>handleButtonClick(2)} className='w-full font-mono' >Project Details</button>
                 </div>
                 {expanded2 === true && (
+                  <>
+                    <div className="container mx-auto">
+                      <div className="bg-white py-2 overflow-auto">
+                        <div className="mb-4 text-justify px-5">
+                          <span className="font-semibold">Description:&nbsp;</span>
+                          <span className="block md:inline">
+                          The YouTube Subscription Capstone Project aimed to optimize subscription management, addressing user dissatisfaction with the existing system's inefficiencies. Refining and enhancing existing endpoints to efficiently retrieve subscriber data.
+                          </span>
+                        </div>
+                        <hr className="my-4" />
+                        <div className="mb-4 text-left px-5">
+                          <span className="font-semibold text-left">Features:</span>
+                          <div className='ml-10'>
+                            <li> Subscription Management</li>
+                            <li> Efficient Data Access</li>
+                            <li> Enhanced Error Logging</li>
+                            <li>User Privacy Protection</li>
+                          </div>                          
+                        </div>
+                        <hr className="my-4" />
+                        <div className="mb-4 text-left px-5">
+                          <span className="font-semibold text-left">Tech Used:</span>
+                          <div className='ml-10'>
+                            <li> Node.JS</li>
+                            <li> Express.JS</li>
+                            <li> MongoDB with Mongoose</li>
+                            <li>Visual Studio Code.</li>
+                            <li> Render for deployment with GitHub for code hosting</li>
+                          </div>                          
+                        </div>
+                        <hr className="my-2" />
+                        {/* <div className="mb-4 text-left px-5">
+                          <span className="font-semibold text-left">View the project demo by
+                            <a href
+                              target='_blank'
+                              rel='noreferrer'
+                              className="text-blue-600 ml-1"
+                            >
+                              clicking here
+                            </a> 
+                          </span>                          
+                        </div> */}
+                        <div className="mb-4 text-left px-5">
+                          <span className="font-semibold text-left">View the project certificate by
+                            <a href='https://certificates.almabetter.com/en/verify/71935460387591'
+                              target='_blank'
+                              rel='noreferrer'
+                              className="text-blue-600 hover:text-red-600 ml-1"
+                            >
+                               clicking here.
+                            </a>
+                          </span>                          
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
+                
+              </div>
+              <div className="container mx-auto px-2 mt-3">
+                <div className="w-full flex flex-wrap justify-between">
+                  <a href='https://youtube-subscription.onrender.com'
+                    target='_blank'
+                    rel='noreferrer'
+                    className="py-3 px-4 bg-blue-600 text-white rounded hover:bg-blue-900 font-mono focus:outline-none focus:bg-blue-900 shadow-md mb-3"
+                  >
+                    SEE LIVE
+                  </a>
+                                    
+                  <a href='#okay'
+                    onClick={isDisabled}
+                    className="py-3 px-4 bg-blue-600 text-white rounded hover:bg-blue-900 font-mono focus:outline-none focus:bg-blue-900 shadow-md mb-3"
+                  >
+                    SOURCE CODE
+                  </a>
+                </div>
+              </div>
+            </div>
+
+
+            <div class='flex-none w-[31%] mx-3 mb-5 max-md:w-full max-lg:w-full  bg-blue-100 rounded-md shadow-md'>
+              <div className='w-full justify-center items-center'>
+                <h1 className='text-2xl m-4 font-medium'>Local Services Portal</h1>
+              </div>
+              <div className='w-full mb-1'>
+                <div className='mx-2 border-2 border-gray-400 rounded'>
+                  <div className='h-52'>
+                    <img 
+                      src={project2} 
+                      alt='Project 2' 
+                      className='w-full h-full' 
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className='w-full px-2'>
+                <div className='bg-blue-600 text-white p-3 hover:bg-blue-900 font-mango shadow-md'>
+                  <button type='submit' onClick={()=>handleButtonClick(6)} className='w-full font-mono' >Project Details</button>
+                </div>
+                {expanded6 === true && (
                   <>
                     <div className="container mx-auto">
                       <div className="bg-white py-2 overflow-auto">
